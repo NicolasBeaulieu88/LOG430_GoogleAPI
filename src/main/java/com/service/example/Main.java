2e8eaa324a3927150abee6d136c6396fa8b76bf9
Nicolas <nic.nicolas.beaulieu@gmail.com>
2021-10-06 10:09:09 -0400

    renaming ExampleApplicaton to Main

--- src/main/java/com/service/example/Main.java
+++ src/main/java/com/service/example/Main.java
@@ -3,15 +3,14 @@ package com.service.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class ExampleApplication {
+public class Main {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExampleApplication.class, args);
+		SpringApplication.run(Main.class, args);
 		SpringApplication.run(GoogleMapsService.class, args);
 	}
 
